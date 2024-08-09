66c0dbc6c1cf45cd62b3a2ca6ed3fb625cd94ff8
MuhammadSameerShahid0 <muhammadsameershahid0@gmail.com>
2024-08-10 01:30:12 +0500

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace IdentityAuthentication.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
